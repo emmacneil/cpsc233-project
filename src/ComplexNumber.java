c3241b94550b3b2dda964e50ee20d47f2d10b7aa
macneil.evan <macneil.evan@zone40-ta.cs.ucalgary.ca>
2019-10-04 12:23:01 -0600

    Fixed the constructor

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -3,8 +3,10 @@ public class ComplexNumber {
 	public double real;
 	public double imaginary;
 	
-	ComplexNumber (ComplexNumber other)
+	ComplexNumber (double r, double i)
 	{
+		real = r;
+		imaginary = i;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
